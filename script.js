fd30295830b00818dd8f2789eac164306199bf09
Mariano125 <mariano.91.duarte@gmail.com>
2023-03-23 22:42:02 -0300

    primer commit reparacion 2

--- script.js
+++ script.js
@@ -17,3 +17,52 @@ function efectoHabilidades(){
 window.onscroll = function(){
     efectoHabilidades();
  }
+
+
+ let btnUno = document.querySelector('.btn-1'); 
+
+btnUno.addEventListener('click', mensaje);
+
+function mensaje() {
+    alert('Lo sentimos, link en reparacion.')
+};
+
+let btnDos = document.querySelector('.btn-2'); 
+
+btnDos.addEventListener('click', mensaje);
+
+function mensaje() {
+    alert('Lo sentimos, link en reparacion.')
+};
+
+let btnTres = document.querySelector('.btn-3'); 
+
+btnTres.addEventListener('click', mensaje);
+
+function mensaje() {
+    alert('Lo sentimos, link en reparacion.')
+};
+
+let btnCuat = document.querySelector('.btn-4'); 
+
+btnCuat.addEventListener('click', mensaje);
+
+function mensaje() {
+    alert('Lo sentimos, link en reparacion.')
+};
+
+let btnCin = document.querySelector('.btn-5'); 
+
+btnCin.addEventListener('click', mensaje);
+
+function mensaje() {
+    alert('Lo sentimos, link en reparacion.')
+};
+
+let btnSeis = document.querySelector('.btn-6'); 
+
+btnSeis.addEventListener('click', mensaje);
+
+function mensaje() {
+    alert('Lo sentimos, link en reparacion.')
+};
